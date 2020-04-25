cd880b1986759dc95705a6497e7afb2e86c66988
Abhishek Rautela <35336369+abhishekrautela@users.noreply.github.com>
2020-04-25 12:33:48 +0530

    Delete params.py

--- params.py
+++
@@ -1,6 +0,0 @@
-import requests as rq
-res = rq.get("http://icanhazdadjoke.com/search",
-              headers={"Accept":"application/json"},
-              params={"term":"dog"}
-                       )
-print(res.json())
